5ef68f9adabcd53a3c4dba7568023091676ffa46
lakshmikantharava1986 <84151704+lakshmikantharava1986@users.noreply.github.com>
2022-04-21 15:21:39 +0530

    commiting challenge 3

---
+++ challenge3/getnestedobj.ps1
@@ -0,0 +1,11 @@
+  
+  #took reefernce of https://docs.microsoft.com/en-us/rest/api/azure/devops/build/builds/get?view=azure-devops-rest-6.0 to get the object
+  $obs=""
+  #this are the keys with a array
+ $key=@('lastChangedBy','_links','avatar')
+  $obs="`$actobj"
+ foreach($val in $key){
+ $obs+="."+$val
+ }
+ #invoke expression
+ Invoke-Expression $obs
